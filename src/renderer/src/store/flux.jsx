0eec7e9d665fe1d7b8503605eceb8e93882fe051
metantonio <antonio_martinez88@hotmail.com>
2024-08-29 12:40:46 -0400

    upd

--- src/renderer/src/store/flux.jsx
+++ src/renderer/src/store/flux.jsx
@@ -78,14 +78,16 @@ const getState = ({ getStore, getActions, setStore }) => {
                 });
             },
 			init: () => {
+				let store = getStore();
+				let actions = getActions();
 				console.log('Iniciando función init()');
                 window.api.ipcRenderer.on('env-variables', (env) => {
 					window.electron.ipcRenderer.send('about to read environment variables')
-					console.log(env)
-                    setStore({ BASE_URL: env.baseURL });
-					setStore({ BASE_URL2: env.baseURL2 });
+					console.log("vars: ", env)
+                    setStore({...store, BASE_URL: env.baseURL });
+					setStore({...store, BASE_URL2: env.baseURL2 });
 					window.electron.ipcRenderer.send('finish to read environment variables')
-                    console.log('Base URL cargada en flux:', env.BASE_URL);
+                    console.log('Base URL cargada en flux:', store.BASE_URL);
                 });
             },
 			useFetch: async (endpoint, data, metodo = "GET") => {
